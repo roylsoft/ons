b12ab757e7ef26b1a3acb6d7cd1d9878815f43dc
Leroy <167892274+roylsoft@users.noreply.github.com>
2024-05-10 12:15:42 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ import cookieParser from "cookie-parser";
 const app = express()
 
 app.use(cors({
-    origin: ['https://ons-client.vercel.app','https://admin-rust-gamma.vercel.app'],
+    origin: ['https://admin-rust-gamma.vercel.app','https://ons-client.vercel.app'],
     methods:[ 'POST','GET','PUT','DELETE'],
     credentials: true
 }))
