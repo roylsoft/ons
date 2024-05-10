818f6ab6d37363b8f4cad1044804ec177ddb8e32
Leroy <muskleroy@gmail.com>
2024-05-10 13:27:21 +0100

    route

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ import cookieParser from "cookie-parser";
 const app = express()
 
 app.use(cors({
-    origin: ['https://ons-client.vercel.app','https://admin-rust-gamma.vercel.app'],
+    origin: ['https://nfonaponline-gnkmgtnqw-roylsofts-projects.vercel.app','https://admin-rust-gamma.vercel.app'],
     methods:[ 'POST','GET','PUT','DELETE'],
     credentials: true
 }))
